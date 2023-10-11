3d78304e59ed8b99bc6968514f967223e883980c
jkondo14 <146896972+jkondo14@users.noreply.github.com>
2023-10-10 17:35:01 -0700

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> engine = new ArrayList<String> ();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Hello");
+        } else if (url.getPath().equals("/search")) {
+            return String.format("%s", engine.toString());
+        } else {
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    engine.add(parameters[1]);
+                    return String.format("Added!");
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
